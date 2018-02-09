fa41ad089f4320492d21b4b6ef7d513244d503a5
Mayank <kumar.mayank98@gmail.com>
2018-02-09 10:37:38 +0530

    Phaser Physics

--- src/states/Play.js
+++ src/states/Play.js
@@ -6,16 +6,20 @@ import Shoot from "../objects/Widgets/sprites/Shoot";
 import GAME_CONST from "../const/GAME_CONST";
 import Meteor from "../objects/widgets/sprites/Meteor";
 import TextUtil from "../util/TextUtil";
+import PlayButton from "../objects/widgets/buttons/PlayButton";
 
 export class Play extends Phaser.State {
     preload() {
         console.log("Preloading Play State");
         this.spaceShipThrust = GAME_CONST.SPEED.SPACE_SHIP;
         this.centerPoint = new Phaser.Point(this.world.centerX, this.world.centerY);
+        this.game.physics.arcade.checkCollision.down = false;
+        this.game.physics.arcade.checkCollision.up = false;
+        this.game.physics.arcade.checkCollision.right = false;
+        this.game.physics.arcade.checkCollision.left = false;
         this.lastTimeStamp = Date.now();
         this.difficulty = 1000;
         this.score = 0;
-        this.healthMetric = 100;
     }
 
     create() {
@@ -24,11 +28,59 @@ export class Play extends Phaser.State {
         this._createSpaceShip();
         this._createScoreText();
         this.game.input.onTap.add(this._changeShipDirection, this);
+        this.projectiles = this.game.add.group();
+        this.meteors = this.game.add.group();
+        this.game.stage.addChild(this.projectiles);
+        this.game.stage.addChild(this.meteors);
     }
 
     update() {
         if(this.healthMetric > 0) {
             this.spaceShip.rotation += this.spaceShipThrust;
+            this.meteors.forEach(this.game.physics.arcade.moveToObject,this.game.physics.arcade, false, this.planet, 200);
+            if (Date.now() - this.lastTimeStamp > this.difficulty) {
+                let position;
+                let prob = Math.random();
+                if (prob < 0.25) {
+                    position = new Phaser.Point(Math.random() * 1080, -256);
+                }
+                else if(prob < 0.5) {
+                    position = new Phaser.Point(1080 + 256, 1920 * Math.random());
+                }
+                else if(prob < 0.75) {
+                    position = new Phaser.Point(1080 * Math.random(), 1920 + 256);
+                }
+                else {
+                    position = new Phaser.Point(-256, 1920 * Math.random());
+                }
+                let meteor = new Meteor({
+                    game: this.game,
+                    posX: position.x,
+                    posY: position.y,
+                    label: 'meteor',
+                    angle: position.angle(this.centerPoint, true),
+                    anchorX: 0.5,
+                    anchorY: 0.5
+                });
+                this.meteors.add(meteor);
+                this.lastTimeStamp = Date.now();
+            }
+            this.game.physics.arcade.collide(this.meteors, this.planet, function(planet, meteor) {
+                meteor.destroy();
+                this.healthMetric-=12.5;
+                this._decreaseHealth();
+                window.navigator.vibrate(1000);
+                if(this.healthMetric <= 0 ) {
+                    this._endGame();
+                }
+                this.game.camera.shake(0.005, 500);
+            }, null, this);
+            this.game.physics.arcade.collide(this.meteors, this.projectiles, function(shoot, meteor) {
+                meteor.kill();
+                shoot.kill();
+                this.score+=1;
+                this._changeScore();
+            }, null, this);
         }
     }
 
@@ -36,6 +88,10 @@ export class Play extends Phaser.State {
         for (let i = this.game.stage.children.length - 1; i >= 0; i--) {
             this.game.stage.removeChild(this.game.stage.children[i]);
         }
+    }
+
+    _changeScore() {
+        this.scoreText.text=this.score;
     }
 
     _createHealth() {
@@ -73,6 +129,7 @@ export class Play extends Phaser.State {
 
     _changeShipDirection() {
         this.spaceShipThrust *= -1;
+        this._shootProjectile();
         this.game.camera.shake(0.005, 300);
     }
 
@@ -92,5 +149,74 @@ export class Play extends Phaser.State {
             anchorY: 0
         });
         this.game.stage.addChild(this.scoreText);
+    }
+
+    _shootProjectile() {
+        let position = this.spaceShip.previousPosition;
+        let shoot = new Shoot({
+            game: this.game,
+            posX: position.x,
+            posY: position.y,
+            label: 'shoot',
+            angle: position.angle(this.centerPoint, true),
+            anchorX: 0.5,
+            anchorY: 0.5
+        });
+        this.projectiles.add(shoot);
+    }
+
+    _decreaseHealth() {
+        this.health && this.health.clear();
+        this.health && this.health.beginFill(0XFFBC3E);
+        this.health && this.health.arc(this.world.centerX, this.world.centerY, 130, this.game.math.degToRad(-90.00),
+            this.game.math.degToRad(((100-this.healthMetric)/100.0)*360 - 90), true, 360);
+    }
+
+    _endGame() {
+        // kapow.invokeRPC('postScore', {"score": this.score}, function() {
+        //     console.log("Success Posting Score");
+        // }, function(error) {
+        //     console.log("Failure Posting score", error);
+        // });
+        this.gameEndText = TextUtil.createText(this.game, {
+            positionX: this.world.centerX,
+            positionY: 560,
+            message: "Game Over",
+            align: "center",
+            backgroundColor: "#000000",
+            fill: "#fefefe",
+            font: 'nunito-regular',
+            fontSize: "80px",
+            fontWeight: 800,
+            wordWrapWidth: 355,
+            anchorX: 0.5,
+            anchorY: 0
+        });
+        this.game.stage.addChild(this.gameEndText);
+        this._createPlayButton();
+        this._drawScoreboard();
+    }
+
+    _createPlayButton() {
+        this.playButtton = new PlayButton({
+            game: this.game,
+            posX: this.world.centerX,
+            posY: this.world.centerY,
+            label: 'playGame',
+            anchorX: 0.5,
+            anchorY: 0.5,
+            callback: this.shutdown
+        });
+        this.game.stage.addChild(this.playButtton);
+    }
+
+    _drawScoreboard() {
+        this.scoreboard = this.game.add.button(this.game.world.centerX, 1800, "scoreboard", this._renderScoreboard, this);
+        this.scoreboard.anchor.setTo(0.5, 0);
+        this.game.stage.addChild(this.scoreboard);
+    }
+
+    _renderScoreboard() {
+        console.log("scoreboared clicked");
     }
 }
